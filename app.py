3090a89ac29dd3c3197fa588720405456b448832
henrique-zz <rickramoos1@gmail.com>
2025-05-09 21:25:54 -0300

    Adicionada a função saudacoes.

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import re
 
 app = Flask(__name__)
 
+PALAVRAS_SAUDACOES = [
+    r"\boi\b",
+    r"\bol[aá]\b"
+]
+
 PALAVRAS_RAM = [
     r"\btela\b.*\bazul\b", # \b pra delimitar pra não pegar "estrelado" por exemplo, e o .* serve pra dizer que pode ter qualquer coisa no meio
     r"\bn[aã]o liga\b",
@@ -18,6 +23,11 @@ class SuporteTecnico(KnowledgeEngine):
     def __init__(self):
         super().__init__()
         self.resposta = "❌ Problema não identificado."
+
+    @Rule(Cliente(problema=P(lambda x: any(re.search(p, x) for p in PALAVRAS_SAUDACOES))))
+    def saudacoes(self):
+        self.resposta = "😀 Olá! Como posso ajudar?"
+        self.halt()
 
     @Rule(Cliente(problema=P(lambda x: any(re.search(p, x) for p in PALAVRAS_RAM))))
     def problema_memoria(self):
